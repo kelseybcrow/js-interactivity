624fabd809f9a5071f0daf36fe6d850b39f44c64
Kelsey Crow <kelseybcrow@gmail.com>
2021-11-11 08:57:34 -0700

    pretty much done

--- index.js
+++ index.js
@@ -2,23 +2,27 @@ console.log("hello world");
 
 const message = document.querySelector("#message");
 
-// addMovie function //
+////// addMovie function //////
 const addMovie = (event) => {
   event.preventDefault();
 
+  ////// getting and setting the value of our input field //////
   let inputField = document.querySelector("input");
 
+  ////// making the html for our movie list items //////
   const movie = document.createElement("li");
 
+  ////// putting the user's input into our span //////
   const movieTitle = document.createElement("span");
   movieTitle.textContent = inputField.value;
 
-  // cross off movie //
+  ////// crossing off a movie //////
   movieTitle.addEventListener("click", crossOffMovie);
 
+  ////// appending the movieTitle-child to our movie-element-parent //////
   movie.appendChild(movieTitle);
 
-  // deleting movie //
+  ////// button fordeleting a movie //////
   const deleteBtn = document.createElement("button");
 
   deleteBtn.textContent = "x";
@@ -27,12 +31,14 @@ const addMovie = (event) => {
 
   movie.appendChild(deleteBtn);
 
+  ////// attaching our movie element to the list already in our html //////
   const list = document.querySelector("ul");
   list.appendChild(movie);
 
   inputField.value = "";
 };
 
+////// selecting the form element and listening for a submit event, then running the addMovie function //////
 document.querySelector("form").addEventListener("submit", addMovie);
 
 const deleteMovie = (event) => {
@@ -41,7 +47,9 @@ const deleteMovie = (event) => {
   message.textContent = "Movie has been deleted!";
 };
 
+////// crossing off a movie //////
 const crossOffMovie = (event) => {
+  ////// being able to 'un-cross-off' a movie //////
   event.target.classList.toggle("checked");
 
   if (event.target.classList.contains("checked")) {
